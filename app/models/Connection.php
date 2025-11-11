4974fd422a13032fd08678afae20fa6e68c5f51d
Niel Casafranca <ncasafranca@senati.pe>
2025-11-11 08:50:39 -0500

    Conexion exitosa a la base de datos

---
+++ app/models/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+$env = parse_ini_file(".env");
+
+$host=$env["DB_HOST"];
+$port=$env["DB_PORT"];
+$db_name=$env["DB_NAME"];
+$user=$env["DB_USER"];
+$pass=$env["DB_PASS"];
+
+class Connection {
+    private static $pdo;
+    
+    public static function conectar() {
+        global $host, $port,$db_name, $user, $pass;
+
+        $dsn="mysql:host=$host;port=$port;dbname=$db_name";
+
+        self::$pdo = new PDO($dsn, $user, $pass);
+
+        if(!isset(self::$pdo)) {
+            return self::$pdo;
+        }
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once 'app/models/Connection.php';
+
+    Connection::conectar();
+
+?>
